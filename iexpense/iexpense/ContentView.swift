77e0cc4ab801e1e397f4e046a1d1db4181c06628
Tan Tran <tantt.dev@gmail.com>
2022-04-27 09:04:23 -0400

    Added project

---
+++ iexpense/iexpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iexpense
+//
+//  Created by Tan Tan on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iexpense/iexpense/iexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iexpenseApp.swift
+//  iexpense
+//
+//  Created by Tan Tan on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct iexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
