9691b3265990d83bb71904af72d4744bef7c38b2
Tan Tran <tantt.dev@gmail.com>
2022-04-28 12:10:12 -0400

    Add `PageView`

---
+++ iexpense/iexpense/PageView.swift
@@ -0,0 +1,25 @@
+//
+//  PageView.swift
+//  iexpense
+//
+//  Created by Tan Tan on 4/28/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        TabView {
+            NumberPadView()
+            ExpenseListView()
+        }
+        .tabViewStyle(.page)
+    }
+}
+
+struct PageViewPreview_Provider: PreviewProvider {
+    static var previews: some View {
+        PageView()
+    }
+}
