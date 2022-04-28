9dfc7aae3a076b0360387546089eba2cc7a4758b
Tan Tran <tantt.dev@gmail.com>
2022-04-28 11:56:54 -0400

    Extract `ExpenseListView` to its own file

--- iexpense/iexpense/ContentView.swift
+++ iexpense/iexpense/ContentView.swift
@@ -6,48 +6,11 @@
 //
 
 import SwiftUI
-import RealmSwift
 
 struct ContentView: View {
-    @State var expenseValue: String = ""
-    @State var category: ExpenseCategory = .other
-    
-    @ObservedResults(ExpenseModel.self) var items
-    
-    var body: some View {
-        VStack {
-            List {
-                ForEach (items, id: \.id) { item in
-                    HStack {
-                        Text(item.category.icon)
-                        let value = String(format: "$%.2f", item.value)
-                        Text(value)
-                    }
-                }
-            }
-            
-            HStack {
-                TextField("New Item", text: $expenseValue)
-                    .textFieldStyle(.roundedBorder)
-                    .keyboardType(.numberPad)
-                
-                Picker("", selection: $category) {
-                    ForEach(ExpenseCategory.allCases, id: \.self) { cat in
-                        Text(cat.title)
-                            .tag(cat)
-                    }
-                }
-                .pickerStyle(.menu)
 
-                Button {
-                    let item = ExpenseModel(value: Double(expenseValue) ?? 0.0, category: category)
-                    $items.append(item)
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                }
-            }
-            .padding()
-        }
+    var body: some View {
+        Text("")
     }
 }
 
---
+++ iexpense/iexpense/ExpenseListView.swift
@@ -0,0 +1,58 @@
+//
+//  ExpenseListView.swift
+//  iexpense
+//
+//  Created by Tan Tan on 4/28/22.
+//
+
+import SwiftUI
+import RealmSwift
+
+struct ExpenseListView: View {
+    @State var expenseValue: String = ""
+    @State var category: ExpenseCategory = .other
+    
+    @ObservedResults(ExpenseModel.self) var items
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach (items, id: \.id) { item in
+                    HStack {
+                        Text(item.category.icon)
+                        let value = String(format: "$%.2f", item.value)
+                        Text(value)
+                    }
+                }
+            }
+            
+            HStack {
+                TextField("New Item", text: $expenseValue)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.numberPad)
+                
+                Picker("", selection: $category) {
+                    ForEach(ExpenseCategory.allCases, id: \.self) { cat in
+                        Text(cat.title)
+                            .tag(cat)
+                    }
+                }
+                .pickerStyle(.menu)
+
+                Button {
+                    let item = ExpenseModel(value: Double(expenseValue) ?? 0.0, category: category)
+                    $items.append(item)
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct ExpenseListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseListView()
+    }
+}
