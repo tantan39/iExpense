7dfe197e58d615e7080efcd8b6f668dc5582fd18
Tan Tran <tantt.dev@gmail.com>
2022-05-09 11:11:01 -0400

    Add date getTime() extension

--- iexpense/iexpense/Extensions/Date+Extensions.swift
+++ iexpense/iexpense/Extensions/Date+Extensions.swift
@@ -19,10 +19,25 @@ extension Date {
         } else if calendar.isDateInYesterday(self) {
             day = "Yesterday"
         } else {
-            dateFormatter.dateFormat = "MMM dd - EEEE"
+            dateFormatter.dateFormat = "EEEE \nMMM YYYY"
             return dateFormatter.string(from: self)
         }
         
         return dateFormatter.string(from: self) + " - " + day
+    }
+    
+    func getTime() -> (year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) {
+        var calendar = Calendar.current
+        if let timeZone = TimeZone(identifier: "EST") {
+           calendar.timeZone = timeZone
+        }
+
+        let year = calendar.component(.year, from: self)
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+        let hour = calendar.component(.hour, from: self)
+        let minute = calendar.component(.minute, from: self)
+        let second = calendar.component(.second, from: self)
+        return (year, month, day, hour, minute, second)
     }
 }
