34209631bbd68965b5079c6d08a92bd500475225
Tan Tran <tantt.dev@gmail.com>
2022-05-13 11:54:43 -0400

    Add firebase package

---
+++ iexpense/iexpense/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  iexpense
+//
+//  Created by Tan Tan on 5/13/22.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
