87632e56d1d499c7f869105075ec168868966a00
Tan Tran <tantt.dev@gmail.com>
2022-05-19 11:42:37 -0400

    Update profile view

---
+++ iexpense/iexpense/Views/UserProfileView.swift
@@ -0,0 +1,50 @@
+//
+//  UserProfileView.swift
+//  iexpense
+//
+//  Created by Tan Tan on 5/19/22.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    @Binding var user: User?
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "person.crop.circle")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.gray)
+            
+            Text("ID: " + (user?.id ?? ""))
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
+            
+                .padding(.top, 20)
+
+            Text(user?.displayName ?? "")
+                .font(.title2)
+                .foregroundColor(.gray)
+                .fontWeight(.semibold)
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("Log out")
+                    .foregroundColor(.teal)
+            }
+            
+            
+        }
+        .padding()
+    }
+}
+
+struct UserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfileView(user: .constant(nil))
+    }
+}
