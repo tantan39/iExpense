1d97154d5ab780757983d0fdd69f71e97d75b6a0
Tan Tran <tantt.dev@gmail.com>
2022-06-07 22:13:58 -0400

    cloud function add test function

--- FirebaseBE/functions/index.js
+++ FirebaseBE/functions/index.js
@@ -7,3 +7,7 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.helloWorld = functions.https.onRequest((req,res) => {
+    res.send('Hello world');
+});
